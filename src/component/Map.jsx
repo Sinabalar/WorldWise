d6e7522c8a30f8a4de96be1bf20bc3c50c8d1e8b
Sinabalar <sinabalar666@gmail.com>
2024-11-01 19:45:01 +0330

    feat: Integrate react-leaflet for map functionality
    - Added `leaflet` and `react-leaflet` dependencies to the project.
    - Implemented Map component using `MapContainer`, `Marker`, `Popup`, and `TileLayer` from `react-leaflet`.
    - Updated cities data structure in `cities.json`.
    - Adjusted CSS to use the latest Leaflet styles.

--- src/component/Map.jsx
+++ src/component/Map.jsx
@@ -1,21 +1,45 @@
 import styles from './Map.module.css';
 import {useNavigate, useSearchParams} from "react-router-dom";
+import {MapContainer, Marker, Popup, TileLayer, useMap} from "react-leaflet"
+import {useState} from "react";
+import {useCitiesContext} from "../context/citiesContext.jsx";
+import city from "./City.jsx";
 
-export default function Map() {
-    const [searchParams,setSearchParams] = useSearchParams();
-    const lat = searchParams.get('lat');
-    const lng = searchParams.get('lng');
-    const navigate = useNavigate();
+function Map() {
+    const [searchParams, setSearchParams] = useSearchParams();
+    const mapLat = searchParams.get('lat');
+    const mapLng = searchParams.get('lng');
+    const [mapPosition, setMapPosition] = useState([40, 0]);
+    const {cities} = useCitiesContext();
 
     return (
         <div
             className={styles.mapContainer}
-            onClick={()=>{
-                navigate('form');
-            }}
         >
-            <h1>Map</h1>
-            <h2>position: {lat} {lng}</h2>
+            <MapContainer
+                center={mapPosition}
+                zoom={6}
+                scrollWheelZoom={true}
+                className={styles.map}
+            >
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+                />
+                {
+                    cities.map(city => (
+                            <Marker key={city.id} position={[city.position.lat, city.position.lng]}>
+                                <Popup>
+                                    <span>{city.emoji}</span>
+                                    <span>{city.cityName}</span>
+                                </Popup>
+                            </Marker>
+                        )
+                    )
+                }
+            </MapContainer>
         </div>
     );
 }
+
+export default Map;
